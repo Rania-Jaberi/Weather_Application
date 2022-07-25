0f4bd168ee156c40802f625fbdcc0467f83092c4
rania jaberi <raniajaberi31@gmail.com>
2022-07-25 19:04:53 +0200

    Liste des villes: Recycler View

--- app/src/main/java/com/example/weather/City/CityAdapter.kt
+++ app/src/main/java/com/example/weather/City/CityAdapter.kt
@@ -1,4 +1,46 @@
 package com.example.weather.City
 
-class CityAdapter {
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.weather.R
+
+class CityAdapter(private val cities: List<City>,
+                  private val cityListener: CityAdapter.CityItemListener)
+    :RecyclerView.Adapter<CityAdapter.ViewHolder> (), View.OnClickListener {
+    interface CityItemListener {
+       fun onCitySelected(city: City)
+       fun onCityDeleted(city: City)
+
+    }
+    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+      val cardView = itemView.findViewById<CardView>(R.id.card_view)!!
+      val cityNameView = itemView.findViewById<TextView>(R.id.text_view)!!
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val viewItem = LayoutInflater.from(parent?.context)
+            .inflate(R.layout.item_city,parent,false)
+        return ViewHolder(viewItem)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val city = cities[position]
+        with(holder){
+            cardView.tag=city
+            cardView.setOnClickListener(this@CityAdapter)
+            cityNameView.text=city.name
+        }
+    }
+
+    override fun getItemCount(): Int = cities.size
+
+    override fun onClick(view: View) {
+        when(view.id){
+            R.id.card_view -> cityListener.onCitySelected(view.tag as City)
+        }
+    }
 }
