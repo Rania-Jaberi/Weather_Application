728358394d5516c9898ffb65ce3c952a965e5b18
rania jaberi <raniajaberi31@gmail.com>
2022-07-25 21:26:27 +0200

    liste des villes: Récuperation en BD

--- app/src/main/java/com/example/weather/City/CityfragmentTest.kt
+++ app/src/main/java/com/example/weather/City/CityfragmentTest.kt
@@ -4,15 +4,18 @@ import android.os.Bundle
 import android.view.*
 import android.widget.Toast
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.weather.App
 import com.example.weather.Database
 import com.example.weather.R
 
 
-class CityfragmentTest : Fragment() {
+class CityfragmentTest : Fragment(), CityAdapter.CityItemListener {
     private lateinit var database: Database
     private lateinit var cities: MutableList<City>
-
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var adapter: CityAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -21,12 +24,19 @@ class CityfragmentTest : Fragment() {
         setHasOptionsMenu(true)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
-        val view=inflater?.inflate(R.layout.fragment_city, container,false)
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        val view=inflater.inflate(R.layout.fragment_city, container,false)
+        recyclerView = view!!.findViewById(R.id.cities_recycler_view)
+        recyclerView.layoutManager=LinearLayoutManager(context)
         return view
 
     } //Faire le lien avec le layout
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        adapter= CityAdapter(cities, this)
+        recyclerView.adapter=adapter
+    }
 
     override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
         inflater?.inflate(R.menu.fragment_city, menu )
@@ -68,6 +78,14 @@ class CityfragmentTest : Fragment() {
                 Toast.LENGTH_LONG).show()
 
 
+    }
+
+    override fun onCitySelected(city: City) {
+        TODO("Not yet implemented")
+    }
+
+    override fun onCityDeleted(city: City) {
+        TODO("Not yet implemented")
     }
 
 }
