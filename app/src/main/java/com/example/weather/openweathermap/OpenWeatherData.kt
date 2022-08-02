905d0520839d20edcdb43aa26cc2e1db5882826f
rania jaberi <raniajaberi31@gmail.com>
2022-08-02 16:59:11 +0200

    HTTP: Mappers in kotlin pour isoler OpenWeatherMap

--- app/src/main/java/com/example/weather/openweathermap/OpenWeatherData.kt
+++ app/src/main/java/com/example/weather/openweathermap/OpenWeatherData.kt
@@ -1,2 +1,22 @@
 package com.example.weather.openweathermap
 
+import com.google.gson.annotations.SerializedName
+
+data class WeatherWrapper(
+    val weather: Array<WeatherData>,
+    val main: MainData
+) {
+    class WeatherData(
+        val description: String,
+        val icon: String
+    ) {
+
+    }
+
+    class MainData(
+        @SerializedName("temp") val temperature: String,
+        val pressure: Int,
+        val humidity: Int) {
+
+    }
+}
---
+++ app/src/main/java/com/example/weather/openweathermap/OpenWeatherService.kt
@@ -0,0 +1,4 @@
+package com.example.weather.openweathermap
+
+interface OpenWeatherService {
+}
