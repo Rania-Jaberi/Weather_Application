e578c31d25c60bc801c9a319d7a726ca90a074ad
rania jaberi <raniajaberi31@gmail.com>
2022-08-07 00:53:38 +0200

    Chargement d'une icone météo avec Picasso

--- app/src/main/java/com/example/weather/Weather/WeatherFragment.kt
+++ app/src/main/java/com/example/weather/Weather/WeatherFragment.kt
@@ -6,6 +6,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import android.widget.Toast
 import com.example.weather.App
 import com.example.weather.R
@@ -21,41 +23,72 @@ class WeatherFragment : Fragment() {
 
     companion object {
         val EXTRA_CITY_NAME = "training.kotlin.extras.EXTRA_CITY_NAME"
-        fun newInstance():WeatherFragment = WeatherFragment() }
+        fun newInstance(): WeatherFragment = WeatherFragment()
+    }
 
 
     private val TAG = WeatherFragment::class.java
     private lateinit var cityName: String
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+
+    private lateinit var city: TextView
+    private lateinit var icon: ImageView
+    private lateinit var weatherDescription: TextView
+    private lateinit var temperature: TextView
+    private lateinit var humidity: TextView
+    private lateinit var pressure: TextView
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
         val view = inflater.inflate(R.layout.fragment_weather, container, false)
+        city = view.findViewById(R.id.city)
+        icon = view.findViewById(R.id.weather_icon)
+        weatherDescription = view.findViewById(R.id.weather_description)
+        temperature = view.findViewById(R.id.temperature)
+        humidity = view.findViewById(R.id.humidity)
+        pressure = view.findViewById(R.id.pressure)
+
         return view
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        if(activity?.intent!!.hasExtra(EXTRA_CITY_NAME)) {
+        if (activity?.intent!!.hasExtra(EXTRA_CITY_NAME)) {
             updateWeatherFactory(activity?.intent!!.getStringExtra(EXTRA_CITY_NAME))
         }
     }
 
     private fun updateWeatherFactory(cityName: String?) {
         this.cityName = cityName!!
-       val call = App.weatherService.getWeather("$cityName")
+        val call = App.weatherService.getWeather("$cityName")
         call.enqueue(object : retrofit2.Callback<WeatherWrapper> {
-            override fun onResponse(call: Call<WeatherWrapper>, response: Response<WeatherWrapper>) {
-                response?.body()?.let{
-                   val weather= mapOpenWeatherDataToWeather(it)
-                    Log.i(TAG.toString(), "weather response: $weather" )
+            override fun onResponse(
+                call: Call<WeatherWrapper>,
+                response: Response<WeatherWrapper>
+            ) {
+                response?.body()?.let {
+                    val weather = mapOpenWeatherDataToWeather(it)
+                    updateUi(weather)
+                    Log.i(TAG.toString(), "weather response: $weather")
                 }
             }
 
             override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) {
-                Log.e(TAG.toString() , "could not load city weather" , t)
-                Toast.makeText(activity, "could not load city weather" , Toast.LENGTH_SHORT).show()
+                Log.e(TAG.toString(), "could not load city weather", t)
+                Toast.makeText(activity, "could not load city weather", Toast.LENGTH_SHORT).show()
             }
 
         })
 
+    }
+
+    private fun updateUi(weather: Weather) {
+        weatherDescription.text = weather.description
+        temperature.text = getString(R.string.weather_temperature_value, weather.temperature.toInt())
+        humidity.text = getString(R.string.weather_humidity_value, weather.humidity)
+        pressure.text= getString(R.string.weather_pressure_value, weather.pressure)
     }
 
 
