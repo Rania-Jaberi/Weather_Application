1258aab36dd643c3d2c8af7f6894e50aa5a06a37
rania jaberi <raniajaberi31@gmail.com>
2022-08-06 13:46:46 +0200

    Layout Weather: Mise en forme générale de la Constraint Layout

--- app/src/main/java/com/example/weather/Weather/WeatherFragment.kt
+++ app/src/main/java/com/example/weather/Weather/WeatherFragment.kt
@@ -1,11 +1,19 @@
 package com.example.weather.Weather
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.example.weather.App
 import com.example.weather.R
+import com.example.weather.openweathermap.WeatherWrapper
+import com.example.weather.openweathermap.mapOpenWeatherDataToWeather
+import retrofit2.Call
+import retrofit2.Response
+import javax.security.auth.callback.Callback
 
 
 class WeatherFragment : Fragment() {
@@ -13,16 +21,45 @@ class WeatherFragment : Fragment() {
 
     companion object {
         val EXTRA_CITY_NAME = "training.kotlin.extras.EXTRA_CITY_NAME"
-        fun newInstance() = WeatherFragment()
-    }
+        fun newInstance():WeatherFragment = WeatherFragment() }
 
-    override fun onCreateView(inflater: LayoutInflater,
-                              container: ViewGroup?,
-                              savedInstanceState: Bundle?
-    ): View? {
+
+    private val TAG = WeatherFragment::class.java
+    private lateinit var cityName: String
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_weather, container, false)
         return view
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if(activity?.intent!!.hasExtra(EXTRA_CITY_NAME)) {
+            updateWeatherFactory(activity?.intent!!.getStringExtra(EXTRA_CITY_NAME))
+        }
+    }
+
+    private fun updateWeatherFactory(cityName: String?) {
+        this.cityName = cityName!!
+       val call = App.weatherService.getWeather("$cityName")
+        call.enqueue(object : retrofit2.Callback<WeatherWrapper> {
+            override fun onResponse(call: Call<WeatherWrapper>, response: Response<WeatherWrapper>) {
+                response?.body()?.let{
+                   val weather= mapOpenWeatherDataToWeather(it)
+                    Log.i(TAG.toString(), "weather response: $weather" )
+                }
+            }
+
+            override fun onFailure(call: Call<WeatherWrapper>, t: Throwable) {
+                Log.e(TAG.toString() , "could not load city weather" , t)
+                Toast.makeText(activity, "could not load city weather" , Toast.LENGTH_SHORT).show()
+            }
+
+        })
+
+    }
+
 
 }
+
+
+
--- app/src/main/java/com/example/weather/openweathermap/OpenWeatherService.kt
+++ app/src/main/java/com/example/weather/openweathermap/OpenWeatherService.kt
@@ -4,9 +4,12 @@ import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
 
-private const val API_KEY= "584ca5db5736376bb29f96da377987ba"
+private const val API_KEY = "584ca5db5736376bb29f96da377987ba"
+
 interface OpenWeatherService {
-    @GET("data/2.5/{id}/weather?units=metric&lang=fr")
-    fun getWeather (@Query("q") ciyName: String,
-    @Query("appid") apiKey: String = API_KEY) : Call<WeatherWrapper>
+    @GET("data/2.5/weather?units=metric&lang=fr")
+    fun getWeather(
+        @Query("q") ciyName: String,
+        @Query("appid") apiKey: String = API_KEY
+    ): Call<WeatherWrapper>
 }
